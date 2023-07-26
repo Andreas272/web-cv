4bebddc428f3ef04ab44220a1225c557537fb122
Andreas Valentin Pedersen <andreasvalentin@hotmail.com>
2023-07-26 15:58:02 +0200

    Education

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
 import './App.css';
 
+export default function App() {
+  return (
+    <HomePage className="App" />
+  );
+}
+
+function HomePage(){
+  return (
+    <>
+    <Name />
+    <div className='ParentBlock'>
+    <Portrait />
+    <Description />
+    </div>
+    <Education/>
+    </>
+  )
+}
+
+function Education(){
+  return(
+    <div className='Section'>
+      <h1> Uddannelse</h1>
+      <div className="ParrentBlock">
+        <div className='ChildBlock'>
+          <h2 >Software AAU </h2>
+          <h3> Projekter </h3>
+          <h3>Kurser </h3>
+        </div>
+        <div className='ChildBlock'>
+          <h2> 9/22 - Nu </h2>
+          
+        </div>
+      
+      
+
+      </div>
+      
+
+    </div>
+  )
+}
 
 function Description(){
   return(
     <div className='ChildBlock'>
-      <h2>Software Developer</h2>
-      <p>With a background in physics and mathemathics, I am currently upgrading my software competences 
-         on the Software program at AAU. I am looking for student work while I finish the program.
+      <h2>Studerende på AAU Software</h2>
+      <p> Jeg er tidligere gymnasielærer i Fysik og Matematik, og opgraderer nu mine kompetencer indenfor software 
+        på Softwarestudiet i Aalborg. Jeg leder efter et studiejob, gerne som front-end udvikler.
       </p>
     </div>
   )
@@ -35,23 +77,8 @@ function Name(){
   )
 }
 
-export default function App() {
-  return (
-    <HomePage className="App" />
-  );
-}
 
-function HomePage(){
-  return (
-    <>
-    <Name />
-    <div className='ParentBlock'>
-    <Portrait />
-    <Description />
-    </div>
-    </>
-  )
-}
+
 
 
 
