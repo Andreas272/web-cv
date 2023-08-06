0c4fc12f6d556556fb5297dc7b5d183ba2521ea4
Andreas Valentin Pedersen <andreasvalentin@hotmail.com>
2023-08-06 21:19:07 +0200

    more files and comments

---
+++ src/components/Margin.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} marginClass is used to place the instances of the margin component to the left or right using the grid container.
+ * @returns 
+ */
+export function Margin({marginClass}){
+    return (
+    <div className={marginClass}>
+    </div>
+    )
+  }
+  
---
+++ src/components/Name.js
@@ -0,0 +1,9 @@
+export function Name(){
+    return (
+      <div className="Name">
+        <center>
+          <h3>Andreas Valentin Pedersen</h3>
+        </center>
+      </div>
+    )
+  }
